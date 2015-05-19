7aa5c25828e01eeebcc89bfc7a91b17c25f2066b
Alec Hollingsworth <alec@mailc.net>
2015-05-18 19:00:07 -0700

    add an SForest() constructor, .isEmpty(), and a test

--- sforest.js
+++ sforest.js
@@ -1,1 +1,11 @@
 'use strict';
+
+function SForest() {
+  this.trees = [];
+}
+
+SForest.prototype.isEmpty = function() {
+  return (this.trees.length === 0);
+};
+
+module.exports = SForest;
--- test/sforest_test.js
+++ test/sforest_test.js
@@ -4,3 +4,11 @@ var SForest = require('../sforest');
 var expect = require('chai').expect;
 
 expect(SForest).to.not.equal(null);
+
+describe('the SForest data structure', function() {
+  describe('has a constructor', function() {
+    it('that constructs empty sforests', function() {
+      expect(new SForest().isEmpty()).to.be.true;
+    });
+  });
+});
