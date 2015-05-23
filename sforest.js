fb7ecf54bc771373e3af465aaf47a328e761bbee
Alec Hollingsworth <alec@mailc.net>
2015-05-23 15:57:28 -0700

    add .cons(..), .iter(..), .toString() and some corresponding tests

--- sforest.js
+++ sforest.js
@@ -8,4 +8,35 @@ SForest.prototype.isEmpty = function() {
   return (this.trees.length === 0);
 };
 
+SForest.prototype.cons = function(element) {
+  var sf = new SForest();
+
+  if (this.trees.length < 2) { // Room for just 1 more
+    var tree = {size: 1, value: element, left: null, right: null};
+    sf.trees = [tree].concat(this.trees);
+  }
+
+  return sf;
+};
+
+SForest.prototype.iter = function(fn) {
+  for (var i = 0; i < this.trees.length; i++) {
+    iterTree(fn, this.trees[i]);
+  }
+};
+
+function iterTree(fn, tree) {
+  fn(tree.value); // Trees always have values
+  if (tree.left !== null) {
+    iterTree(fn, tree.left); // Left, then right
+    iterTree(fn, tree.right);
+  }
+}
+
+SForest.prototype.toString = function() {
+  var strings = [];
+  this.iter(function(elt) { strings.push(elt.toString()); });
+  return '[' + strings.join(', ') + ']';
+};
+
 module.exports = SForest;
--- test/sforest_test.js
+++ test/sforest_test.js
@@ -7,8 +7,30 @@ expect(SForest).to.not.equal(null);
 
 describe('the SForest data structure', function() {
   describe('has a constructor', function() {
-    it('that constructs empty sforests', function() {
+    it('that constructs an empty SForest, []', function() {
       expect(new SForest().isEmpty()).to.be.true;
+    });
+  });
+
+  describe('conses just like a list', function() {
+    it('1 cons [] is not empty', function() {
+      var one = new SForest().cons(1);
+      expect(one).to.not.be.null;
+      expect(one.isEmpty()).to.be.false;
+    });
+
+    it('[] prints as "[]"', function() {
+      expect(new SForest().toString()).to.equal('[]');
+    });
+
+    it('1 cons [] prints as "[1]"', function() {
+      var one = new SForest().cons(1);
+      expect(one.toString()).to.equal('[1]');
+    });
+
+    it('1 cons 2 cons [] prints as "[1, 2]"', function() {
+      var oneTwo = new SForest().cons(2).cons(1);
+      expect(oneTwo.toString()).to.equal('[1, 2]');
     });
   });
 });
